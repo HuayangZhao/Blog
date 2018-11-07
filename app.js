d14ce97c319a4ecbf43b4e9cb9c904e529a13832
Mr.Zhy <LiZhenShuai@handsome.com>
2018-11-07 14:58:58 +0800

    通过循环方式注册路由模块

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const path = require('path')
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -9,12 +11,21 @@ app.set('views','./views')
 // 托管静态文件
 app.use(express.static('./node_modules'))
 
-// 引入indexRouter.js路由
-const router = require('./router/indexRouter.js')
-app.use(router)
-// 引入userRouter.js路由
-const router_1 = require('./router/userRouter')
-app.use(router_1)
+// // 引入indexRouter.js路由
+// const router = require('./router/indexRouter.js')
+// app.use(router)
+// // 引入userRouter.js路由
+// const router_1 = require('./router/userRouter')
+// app.use(router_1)
+
+// 以上引入路由模块过于重复 可以使用一下方法循环注册路由模块
+fs.readdir(path.join(__dirname,'./router'),(err,filename)=>{
+    if(err) return console.log('读取路由目录失败')
+    filename.forEach(fname=>{
+        const router = require(path.join(__dirname,'./router',fname))
+        app.use(router)
+    })
+})
 
 
 app.listen(80,()=>{
